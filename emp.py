228f83f4fb4f31b7bea750fc4cc1206f6f714274
Farhan000333 <farhanwahid003@gmail.com>
2025-02-02 20:16:37 +0530

    [Farhan] Add:python script to calculate Part time of an employee

--- emp.py
+++ emp.py
@@ -1,19 +1,28 @@
 import random
 
 def check_attendance():
-	print("Welcome to Employee wages Computation Program on Master Branch")
-	attendance= random.choice([1,0])
-	return attendance
+    print("Welcome to Employee Wages Computation Program")
+    # 0: Absent, 1: Full time, 2: Part time
+    attendance = random.choice([0, 1, 2])
+    return attendance
+
 def emp_daily_wage():
-	wage_per_hr = 20
-	emp_check = check_attendance()
-	
-	if emp_check == 1:
-		daily_wage = wage_per_hr * 8
-		print(f"the employee is present full day so the daily wage  is : {daily_wage}")
-	else:
-		daily_wage = 0
-		print(f"Employee is absent for the day so the daily wage is : {daily_wage}")
+    wage_per_hr = 20
+    emp_check = check_attendance()
+
+    if emp_check == 1:
+        daily_wage = wage_per_hr * 8
+        print(f"Employee is present full day. Daily wage is: {daily_wage}")
+    elif emp_check == 2:
+        daily_wage = wage_per_hr * 8
+        print(f"Employee is present part time. Daily wage is: {daily_wage}")
+    else:
+        daily_wage = 0
+        print(f"Employee is absent. Daily wage is: {daily_wage}")
 
 if __name__ == "__main__":
-	emp_daily_wage()
+    emp_daily_wage()
+
+
+
+
