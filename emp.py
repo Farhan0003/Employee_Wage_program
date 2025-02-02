1e0eb88ea921551c8cb1bf73758b23abb0221866
Farhan000333 <farhanwahid003@gmail.com>
2025-02-02 19:34:55 +0530

    [Farhan] Add:python script to calculate daily wag eof an employee

--- emp.py
+++ emp.py
@@ -1,13 +1,19 @@
 import random
 
 def check_attendance():
-	print("Welcome to Employee  wages Computation Program  on Master  Branch")
-	attendance=random.choice([1,0])
-	if attendance == 1:
-		print("The  employee is present")
+	print("Welcome to Employee wages Computation Program on Master Branch")
+	attendance= random.choice([1,0])
+	return attendance
+def emp_daily_wage():
+	wage_per_hr = 20
+	emp_check = check_attendance()
+	
+	if emp_check == 1:
+		daily_wage = wage_per_hr * 8
+		print(f"the employee is present full day so the daily wage  is : {daily_wage}")
 	else:
-		print("The employee is absent")
+		daily_wage = 0
+		print(f"Employee is absent for the day so the daily wage is : {daily_wage}")
 
-if  __name__=="__main__":
-	check_attendance()
-	print("welcome to the employee wage Computation")
+if __name__ == "__main__":
+	emp_daily_wage()
