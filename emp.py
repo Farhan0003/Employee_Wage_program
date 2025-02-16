ab7a234b8a45630c1ea8739a5d0fc74cf316cf13
Farhan000333 <farhanwahid003@gmail.com>
2025-02-16 20:27:46 +0530

    [Farhan] Add: Python script to calculate Part time using switch case of an employee

--- emp.py
+++ emp.py
@@ -21,15 +21,16 @@ def emp_daily_wage():
     try:
         emp_check = check_attendance()
 
-        if emp_check == 1:
-            daily_wage = wage_per_hr * 8
-            print(f"Employee is present full day. Daily wage is: {daily_wage}")
-        elif emp_check == 2:
-            daily_wage = wage_per_hr * 4
-            print(f"Employee is present part time. Daily wage is: {daily_wage}")
-        else:
-            daily_wage = 0
-            print(f"Employee is absent. Daily wage is: {daily_wage}")
+        match emp_check:
+            case 1:
+                daily_wage = wage_per_hr * 8
+                print(f"Employee is present full day. Daily wage is: {daily_wage}")
+            case 2:
+                daily_wage = wage_per_hr * 4
+                print(f"Employee is present part time. Daily wage is: {daily_wage}")
+            case 0:
+                daily_wage = 0
+                print(f"Employee is absent. Daily wage is: {daily_wage}")
     except Exception as e:
         print(f"An error occurred while calculating daily wage: {e}")
 
@@ -39,4 +40,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
